4bacfd19e5a7fa9dc88c4c23b62810210b09603b
MSRbuddy <meghanadasari222@gmail.com>
2023-04-22 12:56:47 +0530

    Added : Regex to validate Website Address format!

---
+++ WebsiteAddress/WebsiteAddress/Program.cs
@@ -0,0 +1,23 @@
+﻿using System.Text.RegularExpressions;
+
+namespace WebsiteAddress
+{
+    internal class Program
+    {
+        private static void showMatch(string text, string expr)
+        {
+            Console.WriteLine("The Expression: " + expr);
+            MatchCollection mc = Regex.Matches(text, expr);
+            foreach (Match m in mc)
+            {
+                Console.WriteLine(m);
+            }
+        }
+        static void Main(string[] args)
+        {
+            string str = "https://example.com";
+            Console.WriteLine("Matching URL...");
+            showMatch(str, @"^(http|http(s)?://)?([\w-]+\.)+[\w-]+[.com|.in|.org]+(\[\?%&=]*)?");
+        }
+    }
+}
