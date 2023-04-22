a429bcf68c8b20708e16fc2822eafa805f86cc1e
MSRbuddy <meghanadasari222@gmail.com>
2023-04-22 12:55:11 +0530

    Added : Regex to valid Filename with Image Filetype!

---
+++ ImageFiletype/ImageFiletype/Program.cs
@@ -0,0 +1,16 @@
+﻿using System.Text.RegularExpressions;
+
+namespace ImageFiletype
+{
+    internal class Program
+    {
+        public static string ImageSrc = @"^<[a-zA-Z]+(\s+[a-zA-Z]+\s*=\s*(""([^""])""|'([^'])'))\s/>$";
+        static void Main(string[] args)
+        {
+            string str = "<img src=\"test.gif\"/>";
+            Regex r = new Regex(Program.ImageSrc);
+            Console.WriteLine("Enter a FileName of Image Type : ");
+            Console.WriteLine(r.IsMatch(str));
+        }
+    }
+}
