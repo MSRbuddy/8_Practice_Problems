209d754d0c1b9aa62f421bfa8e1420c3fb66978a
MSRbuddy <meghanadasari222@gmail.com>
2023-04-22 12:53:11 +0530

    Added : No.of Occurences of pattern in given string!

---
+++ PatternOccurence/PatternOccurence/Program.cs
@@ -0,0 +1,20 @@
+﻿using System.Text.RegularExpressions;
+
+namespace PatternOccurence
+{
+    public class Program
+    {
+        public int CountCharsUsingRegex(string source, char toFind)
+        {
+            return new Regex(Regex.Escape(toFind.ToString())).Matches(source).Count;
+        }
+       
+        static void Main(string[] args)
+        {
+            string main = "Mary Had A Little Lamb";
+            char toFind = 'L';
+            int actual = _countChars.CountCharsUsingRegex(main, toFind);
+            Assert.Equal(2, actual);
+        }
+    }
+}
