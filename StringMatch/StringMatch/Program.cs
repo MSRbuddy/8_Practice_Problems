bf8d31e0af940f8feb66dc957d3e7d2b00b3d588
MSRbuddy <meghanadasari222@gmail.com>
2023-04-22 12:44:08 +0530

    Added : String matches that has an 'a' followed by two to three 'b'!

---
+++ StringMatch/StringMatch/Program.cs
@@ -0,0 +1,15 @@
+﻿using System.Text.RegularExpressions;
+
+namespace StringMatch
+{
+    internal class Program
+    {
+        public static string myString = @"^ab{2,3}$";
+        static void Main(string[] args)
+        {
+            Regex r = new Regex(Program.myString);
+            Console.WriteLine("Enter a String : ");
+            Console.WriteLine(r.IsMatch(Console.ReadLine()));
+        }
+    }
+}
