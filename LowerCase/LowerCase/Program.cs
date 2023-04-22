da514c19000509959cfddad0541a47201cb9eed8
MSRbuddy <meghanadasari222@gmail.com>
2023-04-22 12:46:35 +0530

    Added : Sequence of Lowercase letters joined by underscore!

---
+++ LowerCase/LowerCase/Program.cs
@@ -0,0 +1,15 @@
+﻿using System.Text.RegularExpressions;
+
+namespace LowerCase
+{
+    internal class Program
+    {
+        public static string myString = @"^[a-z]+_[a-z]+$";
+        static void Main(string[] args)
+        {
+            Regex r = new Regex(Program.myString);
+            Console.WriteLine("Enter a String : ");
+            Console.WriteLine(r.IsMatch(Console.ReadLine()));
+        }
+    }
+}
